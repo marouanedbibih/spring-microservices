1caf0c36b4c9e71a01c99f3f37569462d772d08a
Marouane Dbibih <marouane.dbibih@outlook.com>
2025-07-26 21:18:16 +0100

    Dockerize the backend application

--- docker/init.sql
+++ docker/init.sql
@@ -2,10 +2,17 @@
 CREATE DATABASE IF NOT EXISTS clients_db;
 CREATE DATABASE IF NOT EXISTS cars_db;
 
--- Create user and grant privileges
-CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON clients_db.* TO 'user'@'%';
-GRANT ALL PRIVILEGES ON cars_db.* TO 'user'@'%';
+-- Create users and grant privileges for each service
+CREATE USER IF NOT EXISTS 'client_user'@'%' IDENTIFIED BY 'client_password';
+CREATE USER IF NOT EXISTS 'car_user'@'%' IDENTIFIED BY 'car_password';
+CREATE USER IF NOT EXISTS 'marouane'@'%' IDENTIFIED BY 'dbibih123';
+
+GRANT ALL PRIVILEGES ON clients_db.* TO 'client_user'@'%';
+GRANT ALL PRIVILEGES ON cars_db.* TO 'car_user'@'%';
+
+-- Optionally, grant all privileges to the main MYSQL_USER as well
+GRANT ALL PRIVILEGES ON clients_db.* TO 'marouane'@'%';
+GRANT ALL PRIVILEGES ON cars_db.* TO 'marouane'@'%';
 
 -- Apply changes
 FLUSH PRIVILEGES;
